e35ceb93ec5b07ed4637397b8836068fce001200
Abhi4675 <abhishekshah2020@gmail.com>
2024-01-08 03:20:36 -0500

    first commit

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <img src="https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_300,q_100,fl_lossy,dpr_2.0,c_fit,f_auto,h_300/o0kmd3vxsmfl9dx1xaob" alt="Hehe" />
+            
+        );
+        
+    }
+}
+
+export default Home;
